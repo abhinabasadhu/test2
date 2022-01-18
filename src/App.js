eb52fbb80122661f4d3e6807872d7d596f4712c5
Abhinaba Sadhu <abhinabasadhu@gmail.com>
2022-01-18 18:14:53 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useRef } from "react";
+import { Form, Button, Container } from "react-bootstrap";
+import axios from "axios";
 
 function App() {
+  const inputId = useRef();
+  const inputArtist = useRef();
+  const [loadeddata, setdata] = React.useState();
+  const [loadeddata2, setdata2] = React.useState();
+
+  async function submithandlerID(e) {
+    e.preventDefault();
+    // console.log(entered);
+    await axios({
+      method: "post",
+      url: "http://localhost:3500/test/id",
+      data: { id: inputId.current.value },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((res) => {
+      console.log(res.data);
+      setdata(res.data);
+    });
+  }
+
+  async function submithandlerArtist(e) {
+    e.preventDefault();
+    await axios({
+      method: "post",
+      url: "http://localhost:3500/test/artist",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: {
+        artist: inputArtist.current.value,
+      },
+    }).then((res) => {
+      console.log(res.data);
+      setdata2(res.data);
+    });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Form onSubmit={submithandlerID}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Id</Form.Label>{" "}
+            <Form.Control
+              type="number"
+              placeholder="number"
+              min="1"
+              max="500"
+              ref={inputId}
+            />
+            <Form.Text className="text-muted">
+              <li>{loadeddata && loadeddata.artist}</li>
+              <li> {loadeddata && loadeddata.title}</li>
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <Form onSubmit={submithandlerArtist}>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Artist</Form.Label>{" "}
+            <Form.Control type="text" placeholder="text" ref={inputArtist} />
+            <Form.Text className="text-muted">
+              <li>{loadeddata2 && loadeddata2.title}</li>
+              <li>{loadeddata2 && loadeddata2.id}</li>
+            </Form.Text>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Container>
     </div>
   );
 }
-
 export default App;
